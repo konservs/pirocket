da40990e709540d93c8226062a3bdb349f4640ee
konserv <a@konservs.com>
2017-11-13 16:19:12 +0200

    first drafts

--- pirocket.cpp
+++ pirocket.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <pigpio.h>
+#include <thread>
+
+#define ALTITUDE_THRESHOLD 10
+#define GPIO_PARACHUTE 1
 
 /*
 TO BUILD
@@ -9,8 +13,36 @@ TO RUN
 sudo ./pirocket
 */
 
+int maxAltitude;
+bool parachuteEjected;
+unsigned int i2c_altimeter_handle;
+
+/**
+ * Send signal to eject parachute
+ */
+int ejectParachute(){
+    gpioWrite(GPIO_PARACHUTE, 1);
+}
+
+/**
+ * Altitude received.
+ */
+int receivedAltitude(int currentAltitude) {
+    if((currentAltitude < maxAltitude)&&((maxAltitude - currentAltitude) > ALTITUDE_THRESHOLD)&&(!parachuteEjected)){
+        ejectParachute();
+    }
+}
+
+/**
+ * Entry point - init threads, start sensors poll
+ */
 int main(int argc, char *argv[]) {
+    parachuteEjected = 0;
     if (gpioInitialise() < 0) return 1;
+
+    gpioSetMode(GPIO_PARACHUTE, PI_OUTPUT);
+    i2c_altimeter_handle = i2cOpen(0,0x01,0);
+    //Start the threads
 
     gpioTerminate();
     return 0;
